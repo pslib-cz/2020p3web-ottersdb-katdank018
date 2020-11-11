07241e378faad888e5bf9f6dfc25f0324dabaf74
Kateřina Daňková <u18.407@pslib.cloud>
2020-11-11 22:48:06 +0100

    Odebrání

---
+++ Database01/Pages/DeleteItem.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Database01.Model;
+
+namespace Database01.Pages
+{
+    public class DeleteItemModel : PageModel
+    {
+        private readonly OtterDbContext _context;
+
+        public DeleteItemModel(OtterDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Otter Otter { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            Otter = await _context.Otters
+                .Include(o => o.Founder)
+                .Include(o => o.Mother)
+                .Include(o => o.Place.Location)
+                .Include(o => o.Place).FirstOrDefaultAsync(m => m.TattooID == id);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            Otter = _context.Otters.Find(id);
+            if (Otter != null)
+            {
+                _context.Otters.Remove(Otter);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
