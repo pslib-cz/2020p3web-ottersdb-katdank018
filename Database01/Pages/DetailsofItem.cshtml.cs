8723c9027f2748d0fc8d1c78146d75cf43cb64d5
Kateřina Daňková <u18.407@pslib.cloud>
2020-11-11 23:39:15 +0100

    Vše funguje

---
+++ Database01/Pages/DetailsofItem.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Database01.Model;
+
+namespace Database01.Pages
+{
+    public class DetailsofItemModel : PageModel
+    {
+        private readonly OtterDbContext _context;
+
+        public DetailsofItemModel(OtterDbContext context)
+        {
+            _context = context;
+        }
+
+        public Otter Otter { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            Otter = await _context.Otters
+                .Include(o => o.Founder)
+                .Include(o => o.Mother)
+                .Include(o => o.Place.Location)
+                .Include(o => o.Place).FirstOrDefaultAsync(m => m.TattooID == id);
+
+            if (Otter == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ Database01/Pages/EditItem.cshtml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Database01.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Database01.Pages
+{
+    public class EditItemModel : PageModel
+    {
+        private readonly OtterDbContext _context;
+
+        public EditItemModel(OtterDbContext context)
+        {
+            _context = context;
+        }
+        public string GetUserId()
+        {
+            return HttpContext.User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value ?? default;
+        }
+
+
+        [BindProperty]
+        public Otter otter { get; set; }
+        [BindProperty]
+        public Place Place { get; set; }
+
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            otter = await _context.Otters
+                .Include(v => v.Place.Location)
+                .Include(v => v.Mother)
+                .Include(v => v.Place)
+                .Include(v => v.Founder).AsNoTracking().FirstOrDefaultAsync(m => m.TattooID == id);
+
+
+            if (otter == null)
+            {
+                return NotFound();
+            }
+
+            PlaceNames = new List<SelectListItem>();
+            Mothers = new List<SelectListItem>();
+            Mothers.Add(new SelectListItem("", null));
+            foreach (var item in _context.Places.Include(l => l.Location).AsEnumerable<Place>())
+            {
+                PlaceNames.Add(new SelectListItem($"{item.Name} ({item.Location.Name})", $"{item.LocationId};{item.Name}"));
+            }
+            foreach (var item in _context.Otters.Include(l => l.Mother).AsEnumerable<Otter>())
+            {
+                Mothers.Add(new SelectListItem($"{item.Name}", $"{item.TattooID}"));
+            }
+            return Page();
+        }
+
+        public List<SelectListItem> PlaceNames { get; set; }
+        public List<SelectListItem> Mothers { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            otter.FounderId = GetUserId();
+            string[] data;
+            data = otter.PlaceName.Split(';');
+            otter.LocationId = int.Parse(data[0]);
+            otter.PlaceName = data[1];
+            _context.Attach(otter).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VydraExists(otter.TattooID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool VydraExists(int? id)
+        {
+            return _context.Otters.Any(e => e.TattooID == id);
+        }
+    }
+}
