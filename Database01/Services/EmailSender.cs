8d3d1e8319f2b153332a9f0f0e96fe098938e93a
Kateřina Daňková <u18.407@pslib.cloud>
2020-11-26 23:01:07 +0100

    Chyba autorizace

--- Database01/Services/EmailSender.cs
+++ Database01/Services/EmailSender.cs
@@ -6,6 +6,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Util.Store;
+using Google.Apis.Auth.OAuth2;
+using System.Threading;
+using Google.Apis.Util;
+using MailKit.Security;
 
 namespace Database01.Services
 {
@@ -18,7 +24,7 @@ namespace Database01.Services
             Configuration = configuration;
         }
 
-        public Task SendEmailAsync(string email, string subject, string text)
+        public async Task SendEmailAsync(string email, string subject, string text)
         {
             var message = new MimeMessage();
             message.From.Add(new MailboxAddress(Configuration["EmailSender:FromName"], Configuration["EmailSender:From"]));
@@ -32,15 +38,40 @@ namespace Database01.Services
 
             message.Body = bodyBuilder.ToMessageBody();
 
-            Int32.TryParse(Configuration["EmailSender:Port"], out int port);
-            using var client = new SmtpClient();
-            client.ServerCertificateValidationCallback = (s, c, h, e) => true; // "vždyověření" certifikátu :)
-            client.Connect(Configuration["EmailSender:Server"], port, MailKit.Security.SecureSocketOptions.StartTlsWhenAvailable);
-            client.Authenticate(Configuration["EmailSender:Username"], Configuration["EmailSender:Password"]);
-            client.Send(message);
-            client.Disconnect(true);
+            const string GMailAccount = "katerina.dankova@pslib.cz";
 
-            return Task.FromResult(0);
+            var clientSecrets = new ClientSecrets
+            {
+                ClientId = "324607673470-bi32g8ajvpjo2strovk1p4tt8652cpkr.apps.googleusercontent.com",
+                ClientSecret = "E2U_ftF02cFYFpJmBURXxBuL"
+            };
+
+            var codeFlow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+            {
+                DataStore = new FileDataStore("CredentialCacheFolder", false),
+                Scopes = new[] { "https://mail.google.com/" },
+                ClientSecrets = clientSecrets
+            });
+
+            // Note: For a web app, you'll want to use AuthorizationCodeWebApp instead.
+            var codeReceiver = new LocalServerCodeReceiver();
+            var authCode = new AuthorizationCodeInstalledApp(codeFlow, codeReceiver);
+
+            var credential = await authCode.AuthorizeAsync(GMailAccount, CancellationToken.None);
+
+            if (credential.Token.IsExpired(SystemClock.Default))
+                await credential.RefreshTokenAsync(CancellationToken.None);
+
+            var oauth2 = new SaslMechanismOAuth2(credential.UserId, credential.Token.AccessToken);
+
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.SslOnConnect);
+                await client.AuthenticateAsync(oauth2);
+                client.Send(message);
+                await client.DisconnectAsync(true);
+            }
         }
     }
 }
